fd41dcc19ca51bd3d565a5913604e3ed966c5f2a
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2017-10-20 15:31:40 +0200

    Added inventory title

--- src/pages/character/character.module.ts
+++ src/pages/character/character.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CharacterPage } from './character';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CharacterPage } from './character';
   ],
   imports: [
     IonicPageModule.forChild(CharacterPage),
+    ComponentsModule,
   ],
 })
 export class CharacterPageModule {}
