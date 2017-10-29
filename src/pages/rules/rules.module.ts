17fa870268e52b7ff7bbc8af849176f5c4577f24
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2017-10-29 22:05:32 +0100

    rules page implemented

--- src/pages/rules/rules.module.ts
+++ src/pages/rules/rules.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RulesPage } from './rules';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { RulesPage } from './rules';
   ],
   imports: [
     IonicPageModule.forChild(RulesPage),
+    ComponentsModule
   ],
 })
 export class RulesPageModule {}
